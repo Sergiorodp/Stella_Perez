34c77235263770cd5b8f6c019d6262393cf5d4d3
Sergiorodp <sergio.rod.perez@outlook.es>
2021-01-07 19:04:34 -0500

    Primer adelanto de la pagina

---
+++ Components/navbar/index.js
@@ -0,0 +1,22 @@
+import navStyle from '../../styles/NavStyle/Nav.module.css'
+
+import Link from 'next/Link'
+
+export default function NavBar(){
+    return(
+        <div className={navStyle.mainNavContainer}>
+            <div className={navStyle.navItemsContainer}>
+                <h2>Stella Perez</h2>
+                
+                <div className = {navStyle.opciones}>
+                    <ul className = {navStyle.listItems}>
+                        <li> <Link href='https://vercel.com/dashboard/integrations'> Inicio </Link></li>    
+                        <li><Link href='https://vercel.com/dashboard/integrations'>Sobre mí</Link></li>
+                        <li><Link href='https://vercel.com/dashboard/integrations'>Servicios</Link></li>
+                        <li><Link href='https://vercel.com/dashboard/integrations'>Contacto</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
